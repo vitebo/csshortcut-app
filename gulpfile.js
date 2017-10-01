12ae58268dd5947cb474ed4b86dd792952566156
Vitebo <vitebo51@gmail.com>
2017-10-01 20:43:44 -0300

    Criando página projects

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,35 @@ var connect = require('gulp-connect')
 var imagemin = require('gulp-imagemin');
 
 gulp.task('pug', function() {
-	gulp.src('./src/*.pug')
-			.pipe(pug())
-			.pipe(gulp.dest('./out'))
-			.pipe(connect.reload())
+  gulp.src('./src/*.pug')
+      .pipe(pug())
+      .pipe(gulp.dest('./out'))
+      .pipe(connect.reload())
 })
 
 gulp.task('stylus', function() {
-	gulp.src('./src/assets/styles/*.styl')
-			.pipe(stylus({compress: true}))
-			.pipe(gulp.dest('./out/assets/styles/'))
-			.pipe(connect.reload())
+  gulp.src('./src/assets/styles/*.styl')
+      .pipe(stylus({compress: true}))
+      .pipe(gulp.dest('./out/assets/styles/'))
+      .pipe(connect.reload())
 })
 
 gulp.task('imagemin', function() {
-	gulp.src('src/assets/img/*')
-			.pipe(imagemin())
-			.pipe(gulp.dest('out/assets/img'))
+  gulp.src('src/assets/img/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('out/assets/img'))
 })
 
 gulp.task('watch', function() {
-	gulp.watch(['./src/*.pug', './src/layouts/*.pug', './src/partials/*.pug'], ['pug'])
-	gulp.watch(['./src/assets/styles/*.styl', './src/assets/styles/modules/*.styl'], ['stylus'])
+  gulp.watch(['./src/*.pug', './src/layouts/*.pug', './src/partials/*.pug'], ['pug'])
+  gulp.watch(['./src/assets/styles/*.styl', './src/assets/styles/modules/*.styl'], ['stylus'])
 })
 
 gulp.task('serve', function() {
-	connect.server({
-		root: './out',
-		livereload: true
-	})
+  connect.server({
+    root: './out',
+    livereload: true
+  })
 })
 
 gulp.task('build', ['pug', 'stylus', 'imagemin'])
